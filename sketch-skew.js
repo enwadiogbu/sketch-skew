658b36b2ec780c4a568ddb76d2f981c9830b8dfe
enwadiogbu <emekanwadiogbu.jr@me.com>
2023-10-27 19:07:15 -0700

    created a rects array and added the ({context, width, height}) parameters to the sketch function. The for each loop is added so that an accurate screenshot of the random loops can be taken without a skewed image.

--- sketch-skew.js
+++ sketch-skew.js
@@ -4,23 +4,32 @@ const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [1080, 1080],
+  animate: true,
 };
 
-const sketch = () => {
+const sketch = ({ context, width, height }) => {
   let x, y, w, h;
 
   const num = 20;
   const degrees = -30;
 
+  const rects = [];
+
+  for (let i = 0; i < num; i++) {
+    x = random.range(0, width);
+    y = random.range(0, height);
+    w = random.range(200, 600);
+    h = random.range(40, 200);
+
+    rects.push({ x, y, w, h });
+  }
+
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    for (let i = 0; i < num; i++) {
-      x = random.range(0, width);
-      y = random.range(0, height);
-      w = random.range(200, 600);
-      h = random.range(40, 200);
+    rects.forEach((rect) => {
+      const { x, y, w, h } = rect;
 
       context.save();
       context.translate(x, y);
@@ -30,10 +39,11 @@ const sketch = () => {
       context.stroke();
 
       context.restore();
-    }
-    //these dimensions are proportionate to the dimensions of the canvas
+    });
   };
+  //these dimensions are proportionate to the dimensions of the canvas
 };
+
 const drawSkewedRect = ({ context, w = 600, h = 200, degrees = -45 }) => {
   const angle = math.degToRad(degrees);
   const rx = Math.cos(angle) * w;
